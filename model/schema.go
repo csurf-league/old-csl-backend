9b927f1168697a5c5666144d9f3691c5bdedc98e
robyzzz <robyawyx@gmail.com>
2022-03-01 20:38:29 +0000

    updated pql schema

--- model/schema.go
+++ model/schema.go
@@ -1,12 +1,14 @@
 package model
 
+// For testing
+
 var schema = `
 DROP TABLE IF EXISTS steam_user;
 DROP TABLE IF EXISTS player_stats;
 
 CREATE TABLE IF NOT EXISTS "steam_user" (
 	"id" SERIAL PRIMARY KEY,
-	"steamid" TEXT,
+	"steamid" VARCHAR(32),
 	"personaname" TEXT,
 	"lastlogoff" INTEGER,
 	"profileurl" TEXT,
@@ -24,7 +26,7 @@ CREATE TABLE IF NOT EXISTS "steam_user" (
 
 CREATE TABLE IF NOT EXISTS "player_stats" (
 	"id" SERIAL PRIMARY KEY,
-	"player_steamid" TEXT,
+	"player_steamid" VARCHAR(32),
 	"map_id" INT,
 	"kills" INT DEFAULT 0,
 	"deaths" INT DEFAULT 0,
@@ -44,11 +46,25 @@ CREATE TABLE IF NOT EXISTS "player_stats" (
 	"rounds_won" INT DEFAULT 0,
 	"rounds_lost" INT DEFAULT 0,
 	"mvp" INT DEFAULT 0
-  );
-  
+);
+
+CREATE TABLE IF NOT EXISTS "map" (
+	"id" SERIAL PRIMARY KEY, 
+	"name" VARCHAR(64)
+);
+
+
+ALTER TABLE "player_stats"
+    ADD CONSTRAINT fk_map
+    FOREIGN KEY (map_id) 
+    REFERENCES map (id);
+
+
 INSERT INTO steam_user(steamid, personaname, lastlogoff, profileurl, avatar, avatarmedium, 
 	avatarfull, realname, primaryclanid, timecreated, loccountrycode, gameid, created_at) 
 	VALUES ('steamid','bozo',123,'kkk','a', 'b', 'c', 'yes', '13', 123, 'dd', 123, NOW());
+
+INSERT INTO map(name) VALUES ('xdream');
 
 INSERT INTO player_stats(player_steamid, map_id) VALUES ('76561198226912040',1);
 `

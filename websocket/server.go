961218bc82a00ce0f7f8ac4cfb9ad51807f1eaa5
robyzzz <robyawyx@gmail.com>
2022-03-02 21:04:43 +0000

    IsInAnotherRoom implementation

--- websocket/server.go
+++ websocket/server.go
@@ -31,49 +31,19 @@ var upgrader = &websocket.Upgrader{
 
 // /api/rooms
 func RoomsWebsocket(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	// create client socket
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		// TODO: return error
+		utils.APIErrorRespond(w, utils.NewAPIError(http.StatusInternalServerError, "Something went wrong..."))
 		return
 	}
 
 	log.Println("Someone connected to /rooms")
-
-	reader(socket)
-}
-
-func reader(conn *websocket.Conn) {
-	defer conn.Close()
-	for {
-		// read from frontend
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err.Error())
-			return
-		}
-
-		data := FromJSON(msg)
-		var response []byte
-		if data.Action == "get-rooms" {
-			response, _ = json.Marshal(Lobby.Rooms)
-		}
-
-		// send back msg
-		if err := conn.WriteMessage(msgType, response); err != nil {
-			log.Println(err.Error())
-			return
-		}
-	}
+	communicate(socket)
 }
 
 // /api/rooms/{room_id} - create a client websocket, putting him in this room
 func (room *Room) HandleRoom(w http.ResponseWriter, r *http.Request) {
-	// TODO: check if client is not on another room
-
 	if room.IsFull() {
 		utils.APIErrorRespond(w, utils.NewAPIError(http.StatusBadRequest, "Room is full"))
 		return
@@ -82,8 +52,13 @@ func (room *Room) HandleRoom(w http.ResponseWriter, r *http.Request) {
 	// get steamid by url param TODO: change this to bearer auth?
 	steamid := r.URL.Query().Get("steamid")
 	if len(steamid) == 0 {
-		log.Println("no steamid")
 		utils.APIErrorRespond(w, utils.NewAPIError(http.StatusNotFound, "Invalid steamid"))
+		return
+	}
+
+	// check if client already is in another room
+	if Lobby.IsInAnotherRoom(steamid) {
+		utils.APIErrorRespond(w, utils.NewAPIError(http.StatusBadRequest, "You can only join 1 room"))
 		return
 	}
 
@@ -108,4 +83,41 @@ func (room *Room) HandleRoom(w http.ResponseWriter, r *http.Request) {
 	// run write and read in 2 separate goroutines
 	go client.write()
 	client.read()
+}
+
+// Reads/writes messages to/from /rooms websocket
+func communicate(conn *websocket.Conn) {
+	defer conn.Close()
+	for {
+		// read from frontend
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		data := FromJSON(msg)
+		var response []byte
+		if data.Action == "get-rooms" {
+			response, _ = json.Marshal(Lobby.Rooms)
+		}
+
+		// send back msg
+		if err := conn.WriteMessage(msgType, response); err != nil {
+			log.Println(err.Error())
+			return
+		}
+	}
+}
+
+// Returns true if player is already in another room
+func (hub *RoomsHub) IsInAnotherRoom(steamid string) bool {
+	for _, r := range hub.Rooms {
+		for _, c := range r.Clients {
+			if c.SteamID == steamid {
+				return true
+			}
+		}
+	}
+	return false
 }
